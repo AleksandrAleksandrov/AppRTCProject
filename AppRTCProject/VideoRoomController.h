39e79fb233763f90f74a6ca6f00c74dbbc95ea4a
Aleksandrov Aleksandr <light.it.zp@gmail.com>
2018-09-01 10:42:28 +0300

    first commit

---
+++ AppRTCProject/VideoRoomController.h
@@ -0,0 +1,13 @@
+//
+//  VideoRoomController.h
+//  AppRTCProject
+//
+//  Created by Aleksandr on 8/24/18.
+//  Copyright © 2018 Aleksandr. All rights reserved.
+//
+
+#ifndef VideoRoomController_h
+#define VideoRoomController_h
+
+
+#endif /* VideoRoomController_h */
---
+++ AppRTCProject/VideoRoomController.m
@@ -0,0 +1,9 @@
+//
+//  VideoRoomController.m
+//  AppRTCProject
+//
+//  Created by Aleksandr on 8/24/18.
+//  Copyright © 2018 Aleksandr. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
