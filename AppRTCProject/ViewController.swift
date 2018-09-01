66aeade97f75b717fae2c75c30434297d198dcab
Aleksandrov Aleksandr <light.it.zp@gmail.com>
2018-09-01 21:23:58 +0300

    Add icons, UI improvements

--- AppRTCProject/ViewController.swift
+++ AppRTCProject/ViewController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+protocol SetRoomName {
+    func setRoomName(roomName: String)
+}
 
+class ViewController: UIViewController {
+    
+//    var delegate : ARDAppClientDelegate?
+
+    @IBOutlet weak var roomTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -19,7 +28,14 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let viewController = segue.destination as? VideoRoomController
+        viewController?.roomName = roomTextField.text
+    }
 
-
+    @IBAction func joinButton(_ sender: UIButton) {
+        performSegue(withIdentifier: "VideoCallSegue", sender: roomTextField.text)
+    }
 }
 
